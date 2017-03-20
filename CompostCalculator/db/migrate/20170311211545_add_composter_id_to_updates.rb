7bde8688db23a6db028d424f9d6b4ce542fb407b
Katherine Cassella <kcassella@cs.hmc.edu>
2017-03-20 15:37:43 -0700

    nothing really

---
+++ CompostCalculator/db/migrate/20170311211545_add_composter_id_to_updates.rb
@@ -0,0 +1,4 @@
+class AddComposterIdToUpdates < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- CompostCalculator/db/schema.rb
+++ CompostCalculator/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311200226) do
+ActiveRecord::Schema.define(version: 20170311211858) do
 
   create_table "composters", force: :cascade do |t|
     t.text     "name"
