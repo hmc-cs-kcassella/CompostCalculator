1e8efd06c59228dd2714e595b8ba8dd5320384b3
Drew Summy <dsummy@cs.hmc.edu>
2017-04-05 15:59:43 -0700

    user migration

--- CompostCalculator/db/schema.rb
+++ CompostCalculator/db/schema.rb
@@ -32,13 +32,13 @@ ActiveRecord::Schema.define(version: 20170405052812) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.float    "dryWeight"
     t.float    "wetWeight"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
+    t.float    "dryWeight"
+    t.integer  "ComposterUpdate_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["ComposterUpdate_id"], name: "index_items_on_ComposterUpdate_id"
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
